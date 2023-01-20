e1b31903d818e1e60951c2c3e68866d6af15bf47
angelv18 <angelvasquez0918@gmail.com>
2023-01-19 21:10:46 -0500

    commit

--- sneakpeek_vue/src/components/ProductBox.vue
+++ sneakpeek_vue/src/components/ProductBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="columns">
-  <div class="column is-3 ">
+  <div class="column is-3">
     <div class="box ">
       <figure class="image mb-4">
         <img v-bind:src="product.get_thumbnail">
