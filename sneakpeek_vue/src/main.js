df46fc7a775821e3596c27d2eff8853bfa749ceb
angelv18 <angelvasquez0918@gmail.com>
2023-01-19 20:52:59 -0500

    commit

--- sneakpeek_vue/src/main.js
+++ sneakpeek_vue/src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000', 'https://gentle-wave-66198.herokuapp.com'
+axios.defaults.baseURL = 'https://gentle-wave-66198.herokuapp.com'
 
 createApp(App).use(store).use(router, axios).mount('#app')
